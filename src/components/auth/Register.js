21ae4281b82646a01cd2ab2076090620e7718943
Lorin Jones <Lorin.Jones@gmail.com>
2022-12-07 12:53:23 -0500

    Authorization works

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -7,6 +7,7 @@ export const Register = () => {
     const firstName = useRef()
     const lastName = useRef()
     const username = useRef()
+    const email = useRef()
     const bio = useRef()
     const password = useRef()
     const verifyPassword = useRef()
@@ -21,6 +22,7 @@ export const Register = () => {
                 "username": username.current.value,
                 "first_name": firstName.current.value,
                 "last_name": lastName.current.value,
+                "email": email.current.value,
                 "bio": bio.current.value,
                 "password": password.current.value
             }
@@ -60,6 +62,10 @@ export const Register = () => {
                     <input ref={username} type="text" name="username" className="form-control" placeholder="Username" required />
                 </fieldset>
                 <fieldset>
+                    <label htmlFor="inputEmail">Email Address</label>
+                    <input ref={email} type="text" name="email" className="form-control" placeholder="Email" required />
+                </fieldset>
+                <fieldset>
                     <label htmlFor="inputPassword"> Password </label>
                     <input ref={password} type="password" name="password" className="form-control" placeholder="Password" required />
                 </fieldset>
@@ -68,7 +74,7 @@ export const Register = () => {
                     <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
                 </fieldset>
                 <fieldset>
-                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <label htmlFor="verifyPassword"> Bio </label>
                     <textarea ref={bio} name="bio" className="form-control" placeholder="Let other gamers know a little bit about you..." />
                 </fieldset>
                 <fieldset style={{
